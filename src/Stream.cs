662ba269c9b17e3fc11124dcd93bbcc287df8a3e
Martin Vassor <martin.vassor@epfl.ch>
2014-10-07 17:04:12 +0200

    Setup Backend architecture

---
+++ src/Stream.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cloud14
+{
+    class Stream
+    {
+        IStorage storage;
+
+        public Stream(IStorage storage)
+        {
+            this.storage = storage;
+        }
+
+
+        class StreamController
+        {
+        }
+        class StreamDeliver
+        {
+
+        }
+    }
+}

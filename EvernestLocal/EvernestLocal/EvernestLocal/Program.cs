232b1dc32fc8bc048f1fbbc6c57e461ff586dd7b
Cher-i <ikram.cherigui@laposte.net>
2014-10-13 18:21:09 +0200

    add first files

---
+++ EvernestLocal/EvernestLocal/EvernestLocal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Http;
+
+namespace EvernestLocal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adresseUriElie = "http://exppad.com/api/pull/event/random";
+
+            //create the constructor with post type and few data
+            HttpClient myRequest = new HttpClient(adresseUriElie, "POST");
+            //show the response string on the console screen.
+            Console.WriteLine(myRequest.GetResponse());
+
+
+
+        }
+    }
+}

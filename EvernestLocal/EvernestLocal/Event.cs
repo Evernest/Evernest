96650669a9b8000a7588eb6fdb79b32975d986f9
apiot <axelle.piot@lyart.fr>
2014-10-27 18:26:01 +0100

    rename files

---
+++ EvernestLocal/EvernestLocal/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernestLocal
+{
+    class Event
+    {
+        public int id { get; set; }
+        public string content { get; set; }
+
+        public Event()
+        {
+            
+        }
+
+        public Event(string content)
+        {
+            this.content = content;
+            id = -1;
+        }
+
+        public void ToPrint()
+        {
+            Console.WriteLine("Id : " + id + "; Content : " + content);
+        }
+    }
+}
---
+++ EvernestLocal/EvernestLocal/PushEventsResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernestLocal
+{
+    class PushEventsResponse
+    {
+        public string new_token { get; set; }
+        public List<int> ids { get; set; }
+        public int token_timeout { get; set; }
+
+        public void ToPrint()
+        {
+            Console.WriteLine("New_Token : " + new_token);
+            foreach (int id in ids)
+            {
+                Console.WriteLine("\t Id : " + id);
+            }
+            Console.WriteLine("  Token_Timeout : " + token_timeout);
+        }
+    }
+}

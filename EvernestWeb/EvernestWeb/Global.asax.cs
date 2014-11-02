4c293964d0e30334e82cf777eaf52655862181b3
Corentin Cadiou <contact@cphyc.me>
2014-11-02 16:31:41 +0100

    Integration of WebAPI

--- EvernestWeb/EvernestWeb/Global.asax.cs
+++ EvernestWeb/EvernestWeb/Global.asax.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;  
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
 
 namespace EvernestWeb
 {
@@ -13,6 +15,9 @@ namespace EvernestWeb
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            EvernestWebAPI.WebApiConfig.Register(GlobalConfiguration.Configuration);
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);

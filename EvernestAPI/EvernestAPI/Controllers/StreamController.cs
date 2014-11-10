71ccfda103f83447e3e55fe314f83a4324824f4a
R1kM <aymeric.fr@hotmail.fr>
2014-11-10 18:03:32 +0100

    Separation of the MVC and the API (new project EvernestAPI)

---
+++ EvernestAPI/EvernestAPI/Controllers/StreamController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EvernestAPI.Controllers
+{
+    public class StreamController : ApiController
+    {
+        // GET: api/Stream
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Stream/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Stream
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Stream/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Stream/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ EvernestAPI/EvernestAPI/Models/IdentityModels.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace EvernestAPI.Models
+{
+    // Vous pouvez ajouter des données de profil pour l'utilisateur en ajoutant d'autres propriétés à votre classe ApplicationUser, consultez http://go.microsoft.com/fwlink/?LinkID=317594 pour en savoir davantage.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            // Notez que authenticationType doit correspondre à l'instance définie dans CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Ajouter des revendications d’utilisateur personnalisées ici
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}

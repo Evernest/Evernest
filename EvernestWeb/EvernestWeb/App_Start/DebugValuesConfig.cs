d9d95580c76db54677aecdfd50e4a5bfa4af7ec9
Elie Michel <elie.michel@ens.fr>
2014-11-10 17:26:40 +0100

    Introduce APIv2
    Add routes and controllers
    Try and use EvernestFront although there still are errors

---
+++ EvernestWeb/EvernestWeb/App_Start/DebugValuesConfig.cs
@@ -0,0 +1,14 @@
+﻿using EvernestFront;
+
+namespace EvernestWeb
+{
+    public class DebugValuesConfig
+    {
+        public static void Init()
+        {
+            Users.AddUser("testing");
+            //Process.CreateStream("testing", "coucou");
+            //Process.Push("testing", "coucou", "test");
+        }
+    }
+}

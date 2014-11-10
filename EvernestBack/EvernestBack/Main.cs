a7a4176b300632a5c7f35bb6ee51bd513ec540dd
Alex AUVOLAT <alexis211@gmail.com>
2014-11-10 18:18:02 +0100

    Remove EvernestCloudService and unlink EvernestWeb (it's broken)

--- EvernestBack/EvernestBack/Main.cs
+++ EvernestBack/EvernestBack/Main.cs
@@ -1,13 +1,14 @@
 using System;
 
+
 namespace EvernestBack
 {
     class Test
     {
         static void Main(string[] args)
         {
-            AzureStorageClient client = new AzureStorageClient();
-            Stream s = client.GetStream("test1");
+            //AzureStorageClient client = new AzureStorageClient();
+            //Stream s = client.GetStream("test1");
             Console.Read();
         }
     }

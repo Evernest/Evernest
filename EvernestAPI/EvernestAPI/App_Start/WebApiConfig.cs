d1fe839c4aba6a29749ed391c37537edbc89b357
R1kM <aymeric.fr@hotmail.fr>
2014-11-10 18:11:09 +0100

    Separation of the MVC and the API (new project EvernestAPI)

--- EvernestAPI/EvernestAPI/App_Start/WebApiConfig.cs
+++ EvernestAPI/EvernestAPI/App_Start/WebApiConfig.cs
@@ -13,13 +13,23 @@ namespace EvernestAPI
         public static void Register(HttpConfiguration config)
         {
              config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "{controller}/{id}/{id2}",
+                name: "PullGetRange",
+                routeTemplate: "{controller}/{streamId}/{action}/{id}/{id2}",
                 constraints: new { id = @"\d+", id2 = @"\d+"},
                 defaults: new { id = RouteParameter.Optional, id2 = RouteParameter.Optional }
             );
 
+             config.Routes.MapHttpRoute(
+                 name: "PullGetOne",
+                 routeTemplate: "{controller}/{streamId}/{action}/{id}",
+                 constraints: new { id = @"\d+"},
+                 defaults: new { id = RouteParameter.Optional }
+             );
 
+             config.Routes.MapHttpRoute(
+                 name: "PullGetRandom",
+                 routeTemplate: "{controller}/{streamId}/{action}"
+                 );
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
             // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.

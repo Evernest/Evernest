c76755d9b4dddff784e324c346e2bc98f5eb5465
Nicolas Jeannerod <nicojpub@gmail.com>
2014-11-17 17:55:52 +0100

    API routing in one line. Clean StreamController

--- EvernestAPI/App_Start/WebApiConfig.cs
+++ EvernestAPI/App_Start/WebApiConfig.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
 
 namespace EvernestAPI
 {
@@ -17,38 +11,12 @@ namespace EvernestAPI
             // We'll have to support content-type application/json. <-- TODO
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
-            /**
-             * Stream
-             */
-
-            // GET: /Stream/{streamId}
-            // GET: /Stream/{streamId}/Pull/{random}
-            // GET: /Stream/{streamId}/Pull/{id}
-            // GET: /Stream/{streamId}/Pull/{startId}/{stopId}
-
-            // POST: /Stream/{streamId}/Push
-
             config.Routes.MapHttpRoute(
-                name: "NoArg",
-                routeTemplate: "{controller}/{id}/{action}",
-                constraints: new { },
-                defaults: new { }
+                name: "APIRoute",
+                routeTemplate: "{controller}/{id}/{action}/{arg0}/{arg1}",
+                constraints: new {},
+                defaults: new {action = "Default", arg0 = RouteParameter.Optional, arg1 = RouteParameter.Optional}
                 );
-
-            config.Routes.MapHttpRoute(
-                name: "OneArg",
-                routeTemplate: "{controller}/{id}/{action}/{arg}",
-                constraints: new { },
-                defaults: new { }
-                );
-
-            config.Routes.MapHttpRoute(
-                name: "TwoArg",
-                routeTemplate: "{controller}/{id}/{action}/{firstArg}/{secondArg}",
-                constraints: new { },
-                defaults: new { }
-                );
-
         }
     }
 }

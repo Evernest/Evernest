004c5d48fe36907656c82234bb7e066639472265
dianegalloiswong <diane.gallois-wong@ens.fr>
2014-11-19 10:19:03 +0100

    Add some classes within Contract
    Nothing is done with these classes yet

---
+++ EvernestFront/EvernestFront/Contract/UserAdded.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace EvernestFront.Contract
+{
+    [DataContract]
+    class UserAdded
+    {
+        [DataMember]
+        string UserName;
+        [DataMember]
+        long UserId;
+        [DataMember]
+        string Key; //?
+    }
+}

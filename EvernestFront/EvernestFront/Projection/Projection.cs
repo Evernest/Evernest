c0b37ef4a8ca90832458d30a4cd3a6b6266691f7
dianegalloiswong <diane.gallois-wong@ens.fr>
2014-11-19 10:46:32 +0100

    Add some more classes (draft)

---
+++ EvernestFront/EvernestFront/Projection/Projection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernestFront.Projection
+{
+    static class Projection
+    {
+        static Tables Tables;
+    }
+
+}
---
+++ EvernestFront/EvernestFront/Projection/Tables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+
+namespace EvernestFront.Projection
+{
+    class Tables
+    {
+        readonly ImmutableDictionary<KeyValuePair<long, long>, AccessRights> RightTable;
+        readonly ImmutableDictionary<long, List<long>> UsersOfStream;
+        readonly ImmutableDictionary<long, List<long>> StreamsOfUser;
+    }
+}

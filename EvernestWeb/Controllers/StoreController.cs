06166d0b88d2fd3d56811ad438c17854b4e51b28
apiot <axelle.piot@lyart.fr>
2014-11-24 00:31:06 +0100

    add files, pages and begin to modify connexion protocol

---
+++ EvernestWeb/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using EvernestWeb.Models;
+
+namespace EvernestWeb.Controllers 
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Stream()
+        {
+            return View();
+        }
+
+        public ActionResult Source()
+        {
+            return View();
+        }
+
+        public ActionResult Coworker()
+        {
+            return View();
+        }
+    }
+}

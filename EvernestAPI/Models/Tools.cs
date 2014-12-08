36fe1fc65aac588e5e5088aaa7047be421a49b1e
Corentin Cadiou <contact@cphyc.me>
2014-12-08 18:07:33 +0100

    Replaced bad try {} catch with if {} sentences

--- EvernestAPI/Models/Tools.cs
+++ EvernestAPI/Models/Tools.cs
@@ -10,37 +10,34 @@ namespace EvernestAPI.Models
     public class Tools
     {
         public static Hashtable parseRequest(System.Net.Http.HttpRequestMessage Request) {
+            /// Parses the URL and the request and raise an exception when 
+            /// it gets invalid data.
+            /// This method doesn't catch any exception, it should be done on the 
+            /// caller side
             var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
-            var nvcHtbl = nvc.Cast<Hashtable>();
             var httpContent = Request.Content;
+            var nvcHtbl = nvc.Cast<Hashtable>();
             var fromBody = httpContent.ReadAsStringAsync().Result; 
 
             Hashtable json = new Hashtable();
             Hashtable body = new Hashtable();
-            try
-            {
+
+            // Get the body
+            if (fromBody.Count() > 0) {
                 body = JsonConvert.DeserializeObject
                     <Hashtable>(fromBody);
+                
             }
-            catch (Newtonsoft.Json.JsonReaderException) 
-            {
-                body = null;
-            }
-            //catch (Newtonsoft.Json.JsonSerializationException)
-            //{
-            //    // TODO : fail with error code
-            //    throw Exception;
-            //}
 
-            // Copy the keys from the URL to the answer
+            // Copy the keys from the URL
             for (int i = 0; i < nvc.Count; i ++ )
             {
                 json.Add(nvc.GetKey(i), nvc.Get(i));
             }
 
-            // Copy the body to the answer
-            try
-            {
+            // Copy the keys from the body if any
+            if (body != null)
+            { 
                 foreach (DictionaryEntry de in body)
                 {
                     try
@@ -53,7 +50,6 @@ namespace EvernestAPI.Models
                     }
                 }
             }
-            catch (NullReferenceException) { }
             return json;    
         }
     }

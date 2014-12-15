650fe9b0ec268c01ba773ee248a162b0ae38ab3f
Nicolas Jeannerod <nicojpub@gmail.com>
2014-12-15 16:03:49 +0100

    Respect VS recommendations

--- EvernestAPI/Models/Tools.cs
+++ EvernestAPI/Models/Tools.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Newtonsoft.Json;
@@ -9,45 +8,43 @@ namespace EvernestAPI.Models
 {
     public class Tools
     {
-        public static Hashtable parseRequest(System.Net.Http.HttpRequestMessage Request) {
+        public static Hashtable ParseRequest(System.Net.Http.HttpRequestMessage request) {
             /// Parses the URL and the request and raise an exception when 
             /// it gets invalid data.
             /// This method doesn't catch any exception, it should be done on the 
             /// caller side
-            var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
-            var httpContent = Request.Content;
+            var nvc = HttpUtility.ParseQueryString(request.RequestUri.Query);
+            var httpContent = request.Content;
             var nvcHtbl = nvc.Cast<Hashtable>();
             var fromBody = httpContent.ReadAsStringAsync().Result; 
 
-            Hashtable json = new Hashtable();
-            Hashtable body = new Hashtable();
+            var json = new Hashtable();
+            var body = new Hashtable();
 
             // Get the body
-            if (fromBody.Count() > 0) {
+            if (fromBody.Any()) {
                 body = JsonConvert.DeserializeObject
                     <Hashtable>(fromBody);
                 
             }
 
             // Copy the keys from the URL
-            for (int i = 0; i < nvc.Count; i ++ )
+            for (var i = 0; i < nvc.Count; i ++ )
             {
                 json.Add(nvc.GetKey(i), nvc.Get(i));
             }
 
             // Copy the keys from the body if any
-            if (body != null)
-            { 
-                foreach (DictionaryEntry de in body)
+            if (body == null) return json;
+            foreach (DictionaryEntry de in body)
+            {
+                try
                 {
-                    try
-                    {
-                        json.Add(de.Key, de.Value);
-                    }
-                    catch (ArgumentException)
-                    {
-                        json[de.Key] = de.Value;
-                    }
+                    json.Add(de.Key, de.Value);
+                }
+                catch (ArgumentException)
+                {
+                    json[de.Key] = de.Value;
                 }
             }
             return json;    

bcd1d0cfa16662d60090a465ba706b9f74ef52dc
apiot <axelle.piot@lyart.fr>
2014-12-15 22:05:37 +0100

    start new site web with mvc5

---
+++ EvernestWeb2/EvernestWeb2/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EvernestWeb2.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult LogOff()
+        {
+            return View();
+        }
+    }
+}

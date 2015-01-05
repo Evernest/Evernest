f9e527eafc7436c1d8969fd0870435d08314cbd6
Martin Vassor <martin.vassor@epfl.ch>
2015-01-05 22:42:41 +0100

    Update stream creation.
    *Warning*, now you should not use RAMStream anymore.
    Please set Dummy to False in app.config
    You should run the Azure Emulator

--- EvernestBack/Main.cs
+++ EvernestBack/Main.cs
@@ -15,35 +15,47 @@ namespace EvernestBack
             catch (ArgumentException e)
             {
                 Console.Write(e.Message);
+                Console.Read();
                 return;
             }
-            System.IO.StreamWriter file = new System.IO.StreamWriter("log.txt");
-
-            const int n = 1000;
-            bool[] tbl = new bool[n+500];
-            for (int i = 0; i < n; i++)
-                tbl[i] = false;
-            for (int i = 0; i < n; i++ )
+            try
             {
-                stream.Push(i.ToString(), pushAgent =>
-                {
-                    stream.Pull(pushAgent.RequestID, pullAgent =>
-                    {
-                        tbl[pullAgent.RequestID] = true;
-                        Console.WriteLine(pullAgent.Message + ". ID : " + pullAgent.RequestID);
-                        file.WriteLine(pullAgent.Message + ". ID : " + pullAgent.RequestID);
-                    });
-                });
-                //System.Threading.Thread.Sleep(100);
-            } //this won't happen with an infinite loop, but anyway, this isn't that important
-            bool ok = false;
-            while(!ok)
+                stream = AzureStorageClient.singleton.GetEventStream("Test2"); // Should fail
+            } catch ( ArgumentException e)
             {
-                ok = true;
-                for( int i = 0; i < 1000 && ok; ok = ok && tbl[i], i++);
+                Console.WriteLine("Success, you failed : ");
+                Console.WriteLine(e.Message);
+                Console.Read();
+                return;
             }
 
-            file.Close();
+            //System.IO.StreamWriter file = new System.IO.StreamWriter("log.txt");
+
+            //const int n = 1000;
+            //bool[] tbl = new bool[n+500];
+            //for (int i = 0; i < n; i++)
+            //    tbl[i] = false;
+            //for (int i = 0; i < n; i++ )
+            //{
+            //    stream.Push(i.ToString(), pushAgent =>
+            //    {
+            //        stream.Pull(pushAgent.RequestID, pullAgent =>
+            //        {
+            //            tbl[pullAgent.RequestID] = true;
+            //            Console.WriteLine(pullAgent.Message + ". ID : " + pullAgent.RequestID);
+            //            file.WriteLine(pullAgent.Message + ". ID : " + pullAgent.RequestID);
+            //        });
+            //    });
+            //    //System.Threading.Thread.Sleep(100);
+            //} //this won't happen with an infinite loop, but anyway, this isn't that important
+            //bool ok = false;
+            //while(!ok)
+            //{
+            //    ok = true;
+            //    for( int i = 0; i < 1000 && ok; ok = ok && tbl[i], i++);
+            //}
+
+            //file.Close();
 
             //i suspect this operation to block Console.WriteLine (thus preventing the other thread to run)
             //so i added a console.read() in the callback to have the time to see the message after pushing enter

b59a40aec96d7180391d3a7efa7d6c742c826fe3
R1kM <aymeric.fr@hotmail.fr>
2015-01-12 17:03:55 +0100

    Remove gotos in RightController

--- EvernestAPI/Controllers/RightController.cs
+++ EvernestAPI/Controllers/RightController.cs
@@ -31,26 +31,26 @@ namespace EvernestAPI.Controllers
             }
 			// Get the Stream
             var getSource = EvernestFront.Source.GetSource(sourceId);
-			if (!getSource.Success)
-			{
-				failed = true;
+            if (!getSource.Success)
+            {
+                failed = true;
                 errorMessage = "Can't access the user";
                 error = getSource.Error;
-                goto end;
-			}
-
-            var source = getSource.Source;
-            // The source haven't access to stream
-            if (source.EventStream.Id != streamId)
-            {
-                accessRight = AccessRights.NoRights; 
             }
             else
             {
-                accessRight = source.Right; 
+                var source = getSource.Source;
+                // The source haven't access to stream
+                if (source.EventStream.Id != streamId)
+                {
+                    accessRight = AccessRights.NoRights;
+                }
+                else
+                {
+                    accessRight = source.Right;
+                }
             }
-			
-            end:
+            
             // BEGIN DEBUG //
             var debug = new Hashtable();
             debug["Controller"] = "Right";
@@ -100,64 +100,70 @@ namespace EvernestAPI.Controllers
                     failed = true;
                     errorMessage = "Can't access the source.";
                     error = getUser.Error;
-                    goto end;
                 }
-                user = getUser.User;
+                else
+                {
+                    user = getUser.User;
+
+                    // Convert the string to an AccessRights enum
+                    switch (right.ToLower())
+                    {
+                        case "none":
+                            accessRight = AccessRights.NoRights;
+                            break;
+                        case "readonly":
+                            accessRight = AccessRights.ReadOnly;
+                            break;
+                        case "writeonly":
+                            accessRight = AccessRights.WriteOnly;
+                            break;
+                        case "readwrite":
+                            accessRight = AccessRights.ReadWrite;
+                            break;
+                        case "admin":
+                            accessRight = AccessRights.Admin;
+                            break;
+                        case "root":
+                            accessRight = AccessRights.Root;
+                            break;
+
+                        default:
+                            // Should never happen
+                            return Request.CreateResponse(HttpStatusCode.InternalServerError);
+                    }
+
+                    // Get the Source
+                    var getSource = EvernestFront.Source.GetSource(sourceId);
+                    if (!getSource.Success)
+                    {
+                        failed = true;
+                        errorMessage = "Can't access the source.";
+                        error = getSource.Error;
+                    }
+                    else
+                    {
+                        // Modifies the accessRights
+                        var answer = user.SetRights(streamId, user.Id, accessRight);
+                        if (!answer.Success)
+                        {
+                            failed = true;
+                            errorMessage = "Can't modify rights.";
+                            error = answer.Error;
+                        }
+                    }
+                }
             }
             else 
             {
                 failed = true;
                 errorMessage = "Missing user id.";
                 error = null;
-                goto end;
             }
 
-			// Convert the string to an AccessRights enum
-			switch (right.ToLower())
-			{
-				case "none":
-					accessRight = AccessRights.NoRights;
-					break;
-				case "readonly":
-					accessRight = AccessRights.ReadOnly;
-					break;
-				case "writeonly":
-					accessRight = AccessRights.WriteOnly;
-					break;
-				case "readwrite":
-					accessRight = AccessRights.ReadWrite;
-					break;
-				case "admin":
-					accessRight = AccessRights.Admin;
-					break;
-				case "root":
-					accessRight = AccessRights.Root;
-					break;
-				
-				default:
-					// Should never happen
-					return Request.CreateResponse(HttpStatusCode.InternalServerError);
-			}
-
-			// Get the Source
-            var getSource = EvernestFront.Source.GetSource(sourceId);
-			if (!getSource.Success)
-			{
-				failed = true;
-                errorMessage = "Can't access the source.";
-                error = getSource.Error;
-                goto end;
-			}
-            // Modifies the accessRights
-            var answer = user.SetRights(streamId, user.Id, accessRight);
-            if (!answer.Success) {
-                failed = true;
-                errorMessage = "Can't modify rights.";
-                error = answer.Error;
-                goto end;
-            }
 			
-            end:
+           
+
+            // end est ici
             // BEGIN DEBUG //
             var debug = new Hashtable();
             debug["Controller"] = "Right";

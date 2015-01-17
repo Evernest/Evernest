062edfa66ffdb48294e454c8e52decada6b3d21f
Elie Michel <elie.michel@ens.fr>
2015-01-17 19:21:29 +0100

    Move Scripts to Content
    And place css files into a Theme subdirectory of Content

--- EvernestWeb/App_Start/BundleConfig.cs
+++ EvernestWeb/App_Start/BundleConfig.cs
@@ -9,24 +9,24 @@ namespace EvernestWeb
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Content/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Content/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Content/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/Scripts/bootstrap.js",
+                      "~/Content/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/EvernestWeb2.css",
-                      "~/Content/Site.css"));
+                      "~/Content/Theme/bootstrap.css",
+                      "~/Content/Theme/EvernestWeb2.css",
+                      "~/Content/Theme/Site.css"));
         }
     }
 }
--- EvernestWeb/README.md
+++ EvernestWeb/README.md
@@ -11,11 +11,11 @@ It runs the website presenting Evernest but also the user manager of the officia
 
 It uses ASP.NET MVC and directory organisation follow the usual conventions of this framework:
 
- * `App_Start`: Contains the code executed at application start.
- * `Content`: Contains static assets (css, images).
- * `Scripts`: Contains static scripts (js).
- * `Models`, Views, Controllers: Usual MVC units.
+ * `Models`, `Views`, `Controllers`: Usual MVC units.
  * `ViewModels`: Models related to the rendering of a specific view.
+ * `Content`: Contains static assets (css, scritps, images, fonts).
+ * `Application`: Misc classes
+ * `App_Start`: Contains the code executed at application start.
 
 Application starts with `Global.asax.cs`.
 

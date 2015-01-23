c5e7c4d1ce3461ecf7fa37c981b771a665233a12
Nicolas Jeannerod <nicojpub@gmail.com>
2015-01-23 13:23:22 +0100

    Update Response controller

--- EvernestAPI/Models/Response.cs
+++ EvernestAPI/Models/Response.cs
@@ -1,11 +1,24 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Collections;
+using System.Web.Http;
 
 namespace EvernestAPI.Models
 {
     internal static class Response
     {
+       
+        public static HttpResponseMessage Error(HttpRequestMessage request, string errorMessage)
+        {
+            var ans = new Hashtable();
+            ans["Success"] = false;
+            var error = new Hashtable();
+            error["Message"] = errorMessage;
+            ans["Error"] = error;
+            return request.CreateResponse(HttpStatusCode.OK, ans); // TODO: find an other Status Code
+        }
+
         public static HttpResponseMessage MissingArgument(HttpRequestMessage request, string argument)
         {
             var ans = new Hashtable();
@@ -18,10 +31,10 @@ namespace EvernestAPI.Models
             error["Argument"] = argument;
             ans["Error"] = error;
 
-            return request.CreateResponse(HttpStatusCode.BadRequest, ans);            
+            return request.CreateResponse(HttpStatusCode.BadRequest, ans); // TODO: find an other Status Code
         }
 
-        public static HttpResponseMessage BadArgument(HttpRequestMessage request, string argument)
+        public static HttpResponseMessage BadArgument(HttpRequestMessage request, string[] arguments)
         {
             var ans = new Hashtable();
 
@@ -30,10 +43,15 @@ namespace EvernestAPI.Models
             var error = new Hashtable();
             error["Message"] = "BadArgument";
             error["HelpMessage"] = ""; // TODO: Fill this field.
-            error["Argument"] = argument;
+            error["Arguments"] = arguments;
             ans["Error"] = error;
 
-            return request.CreateResponse(HttpStatusCode.BadRequest, ans);
+            return request.CreateResponse(HttpStatusCode.BadRequest, ans); // TODO: find an other Status Code
+        }
+
+        public static HttpResponseMessage BadArgument(HttpRequestMessage request, string argument)
+        {
+            return BadArgument(request, new string[] {argument});
         }
 
         public static HttpResponseMessage BadRequest(HttpRequestMessage request)
@@ -50,5 +68,12 @@ namespace EvernestAPI.Models
             return request.CreateResponse(HttpStatusCode.BadRequest, ans);
         }
 
+        public static HttpResponseMessage Success(HttpRequestMessage request, object o)
+        {
+            var ans = new Hashtable();
+            ans["Success"] = true;
+            ans["Response"] = o;
+            return request.CreateResponse(HttpStatusCode.OK, ans);
+        }
     }
 }

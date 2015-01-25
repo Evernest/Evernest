2784a584a1367881d87f815c3dd0b0eac1ada1c5
Nicolas Jeannerod <nicojpub@gmail.com>
2015-01-25 16:17:00 +0100

    Delete UserAdd

--- EvernestAPI/App_Start/WebApiConfig.cs
+++ EvernestAPI/App_Start/WebApiConfig.cs
@@ -67,17 +67,6 @@ namespace EvernestAPI
                     action="New",
                 }
                 );
-
-            config.Routes.MapHttpRoute(
-                name: "APIUserAdd",
-                routeTemplate: "{controller}/Add/{username}/{password}",
-                constraints: new
-                {
-                    username = @"[a-zA-Z0-9]*",
-                    password = @"[a-zA-Z0-9]*",
-                },
-                defaults: new {}
-                );
         }
     }
 }
